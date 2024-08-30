9513571bbafcea4060199f299007f314e136f704
IOANNIS IASON BOIDANIS <61283674+JasonSDMN2001@users.noreply.github.com>
2024-08-30 20:18:29 +0300

    Testing pre-commit hook

--- TicketsApp/Controllers/UsersController.cs
+++ TicketsApp/Controllers/UsersController.cs
@@ -275,7 +275,7 @@ namespace TicketsApp.Controllers
                     ModelState.AddModelError("Email", "Email already in use.");
                     return View(user);
                 }
-
+                hello
                 _context.Add(user);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Login)); 
